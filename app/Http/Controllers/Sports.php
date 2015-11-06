49ecc5ea2a8b6aaf319f8e0591f8da7b62ea6b50
DeryckSPBU <pavlolvandrei@gmail.com>
2015-11-06 16:56:38 +0300

    fix routes

--- app/Http/Controllers/Sports.php
+++ app/Http/Controllers/Sports.php
@@ -9,47 +9,47 @@ use Request;
 class Sports extends BaseController
 {
     public function index()
-	{
-                $sports = Sport::all();
-		return view('sports.index',['sports' => $sports]);
-	}
+    {
+        $sports = Sport::all();
+        return view('sports.index', ['sports' => $sports]);
+    }
 
     public function create()
-	{
-		return view('sports.create');
-	}
+    {
+        return view('sports.create');
+    }
 
     public function store()
-	{
-                $new = Request::only('identity','name', 'count');
-                $sport = Sport::create($new);
-                return redirect('sports');
-	}
+    {
+        $new = Request::only('identity', 'name', 'count');
+        $sport = Sport::create($new);
+        return redirect('sports');
+    }
 
     public function show($id)
-	{
-                $sport= Sport::find($id);
-		return view('sports.show')->with(['sport' => $sport]);
-	}
+    {
+        $sport= Sport::find($id);
+        return view('sports.show')->with(['sport' => $sport]);
+    }
 
     public function edit($id)
-	{
-                $sport = Sport::find($id);
-		return view('sports.edit')->with(['sports' => $sport]);
-	}
+    {
+        $sport = Sport::find($id);
+        return view('sports.edit')->with(['sport' => $sport]);
+    }
 
     public function update($id)
-	{
-                $new = Request::only('identity','name', 'count');
-                $old = Sport::find($id);
-                $old->update($new);
-		return redirect('sports');
-	}
+    {
+        $new = Request::only('identity', 'name', 'count');
+        $old = Sport::find($id);
+        $old->update($new);
+        return redirect('sports');
+    }
 
     public function destroy($id)
-	{
-                $sport = Sport::find($id);
-                $sport->delete();
-		return redirect('sports');
-	}
+    {
+        $sport = Sport::find($id);
+        $sport->delete();
+        return redirect('sports');
+    }
 }
