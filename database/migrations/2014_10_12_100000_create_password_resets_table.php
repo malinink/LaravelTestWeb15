de165a8203c06b5cd2c98f8530b5efb422a71907
malinink <apmath@malinink.ru>
2015-11-11 14:52:35 +0300

    fix password reserts migration

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -5,25 +5,28 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreatePasswordResetsTable extends Migration
 {
- /**
- * Run the migrations.
- *
- * @return void
- */
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token')->index(); 
-            $table->timestamp('created_at');
-        });
+        Schema::create(
+            'password_resets',
+            function (Blueprint $table) {
+                $table->string('email')->index();
+                $table->string('token')->index();
+                $table->timestamp('created_at');
+            }
+        );
     }
 
- /**
- * Reverse the migrations.
- *
- * @return void
- */
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
         Schema::drop('password_resets');
