2a17d5bd1600f42d66f7ab8398941c8c506bbca1
artesby <artesby1@gmail.com>
2015-11-11 20:31:14 +0300

    [cs], validation

---
+++ app/Http/Requests/FruitRequest.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class FruitRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     * 
+     * @return boolean
+     */
+    public function authorize()
+    {
+        return true;
+    }
+    
+    /**
+     * Get the validation rules that apply to the request.
+     * 
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'identity' => 'required|integer|unique:fruits,identity,'.$this->id,
+            'name' => 'required|alpha',
+            'color' => 'required|alpha',
+            'weight' => 'required|numeric',
+            'price' => 'required|numeric',
+        ];
+    }
+}
--- database/migrations/2015_10_29_155706_create_fruit_table.php
+++ database/migrations/2015_10_29_155706_create_fruit_table.php
@@ -5,18 +5,29 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateFruitTable extends Migration
 {
+    /**
+     * Create table.
+     * 
+     * @return void
+     */
     public function up()
     {
-        Schema::create('fruits', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('identity');
-            $table->string('name');
-            $table->string('color');
-            $table->integer('weight');
-            $table->timestamps();
-        });
+        Schema::create(
+            'fruits', function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('identity');
+                $table->string('name');
+                $table->string('color');
+                $table->integer('weight');
+                $table->timestamps();
+            }
+        );
     }
-
+    /**
+     * Delete table.
+     * 
+     * @return void
+     */
     public function down()
     {
         Schema::drop('fruits');
