ea8334cdb1385ced4a5a59eae79b40fa7fdcf2c9
AnanasKelly <anastasiya_johnni@mail.ru>
2015-12-02 20:16:46 +0300

    some fix

--- app/Food.php
+++ app/Food.php
@@ -11,4 +11,10 @@ class Food extends Model
         'name'
     );
     public $timestamps = false;
+    
+    public function whales()
+    {
+    
+        return $this->belongsToMany('App\Whale');
+    }
 }
