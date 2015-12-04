7b6784b479c01580a35877628ed6e6eba60ed8d1
artesby <artesby1@gmail.com>
2015-12-05 00:50:10 +0300

    routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -65,6 +65,7 @@ Route::delete('/fruits/{id}', [
     'as' => 'fruits.destroy', 'uses' => 'Fruits@destroy'
     ]);
 
+Route::resource('collectors', 'Collectors');
 
 Route::get('whale', ['as' => 'whale.index', 'uses' => 'Whales@index']);
 Route::get('whale/create', ['as' => 'whale.create', 'uses' => 'Whales@create']);
